3239dc3de623fa0ff0687ebbcaa60f4e0cb34a0d
param gupta <paramgupta0305@gmail.com>
2026-01-05 02:42:52 +0530

    Fixed 2

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,9 @@
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/api/socket.io" });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+    transports: ["websocket", "polling"],
+  });
 };
